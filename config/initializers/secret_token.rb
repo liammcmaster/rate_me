a0bf06136a72fa8a01988550537d2622f796d88a
Liam McMaster <liam.mcmaster@gmail.com>
2013-04-26 02:05:25 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMe::Application.config.secret_token = 'e9065a621fd6a2f40a894ba459ea4c0caa04ebd32eaf2ee4f30c4b1fbfa866907673deb725900f5d04498449d5e3dc14a48a37168d3deda4f371c2b490a1ca82'
